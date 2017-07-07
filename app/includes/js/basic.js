f7b8a8cf714c20b4a30ca2c4a709b34ebd846bd5
Patrick <mail@patrick-vogt.com>
2017-07-08 00:28:29 +0200

    cleanup

--- app/includes/js/basic.js
+++ app/includes/js/basic.js
@@ -23,20 +23,6 @@ function ExecAjax(program, id, urldata) {
         url: program,
         data: urldata,
         success: function(data) {
-            //$("#progressModal").modal('hide');
-            if (data.match(/CreateThumbnail/g)) {
-                var array = data.split('|');
-                CreateThumbnail('thumbnail-body',array[1],array[2]);
-
-            }
-
-            else if (data.match(/LoadOldThumbnails/g)) {
-              var array = data.split('|');
-              for (var i = 1; i < array.length -1; i++) {
-                CreateThumbnail('thumbnail-body',array[i],array[i]);
-              }
-
-            }
 
             else if (data.match(/ERROR/g)) {
                 var array = data.split('|');
@@ -47,13 +33,9 @@ function ExecAjax(program, id, urldata) {
                 $('#logmessage').html(array[1]);
             }
 
-            else if (data.match(/ADMIN/g)) {
-                var array = data.split('|');
-                $('#adminmessage').html('<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>'+array[1]+'</strong></div>');
-            }
+            
 
             else if (id && $('#' + id)) $('#' + id).html(data);
-            /*      Scripte ausführen mit Ajax     */
             var jScripts = $(data).filter('script');
             $(jScripts).each(function(intI, objScript) {
                 eval($(this).text());
